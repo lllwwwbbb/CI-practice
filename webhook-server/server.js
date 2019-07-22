f66ebef920ae26064b6eececf6eba3be5ed5e8d0
luowebb <151220068@smail.nju.edu.cn>
2019-07-22 23:32:58 +0800

    modify server.js

--- webhook-server/server.js
+++ webhook-server/server.js
@@ -28,7 +28,7 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
-  console.log('modified')
+  console.log(console.log)
   runCommand('sh', ['./auto_build.sh'], function(resp) {
     console.log(resp)
   });
