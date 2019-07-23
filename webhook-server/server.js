c2f672641e52708e1102e6b91e8ce6686e97a2ea
luowebb <151220068@smail.nju.edu.cn>
2019-07-23 12:01:05 +0800

    modify server.js

--- webhook-server/server.js
+++ webhook-server/server.js
@@ -18,7 +18,7 @@ handler.on('error', function (err) {
 });
 
 function runCommand( cmd, args, callback ){
-    var child = spawn( cmd, args );
+    var child = spawn( cmd, args, { stdio: 'inherit' } );
     var resp = '';
     child.stdout.on('data', function( buffer ){ console.log(buffer.toString()) });
     child.stdout.on('close', function(){ callback( resp ) });
