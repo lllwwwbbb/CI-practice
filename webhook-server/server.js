e1707ed0f7cd17a156826739a0a39e5c8ffe307f
luowebb <151220068@smail.nju.edu.cn>
2019-07-23 12:41:57 +0800

    modify server.js

--- webhook-server/server.js
+++ webhook-server/server.js
@@ -1,7 +1,5 @@
 var http = require('http');
 var spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
-var execFile = require('child_process).execFile;
 var createHandler = require('github-webhook-handler');
 
 // 下面填写的myscrect跟github webhooks配置一样，下一步会说；path是我们访问的路径
@@ -18,18 +16,19 @@ handler.on('error', function (err) {
   console.error('Error:', err.message)
 });
 
-function runCommand( cmd, args, callback ){
-    var child = spawn( cmd, args, { stdio: 'inherit' } );
-    var resp = '';
-    child.stdout.on('data', function( buffer ){ console.log(buffer.toString()) });
-    child.stdout.on('close', function(){ callback( resp ) });
+function runCommand (cmd_with_args){
+  vec = cmd_with_args.split(' ')
+  spawn(vec[0], vec.slice(1), { stdio: 'inherit' })
 }
+
 // 监听到push事件的时候执行我们的自动化脚本
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
-  spawn('sh', ['./on_push.sh'], { stdio: 'inherit' })
-  spawn('pm2', ['reload', 'server'], { stdio: 'inherit' })
+  //spawn('sh', ['./on_push.sh'], { stdio: 'inherit' })
+  //spawn('pm2', ['reload', 'server'], { stdio: 'inherit' })
+  runCommand('sh ./on_push.sh')
+  runCommand('pm2 reload server')
 });
 
